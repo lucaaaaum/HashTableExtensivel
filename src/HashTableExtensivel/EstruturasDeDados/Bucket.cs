d5b878af36bc2cd3b1099b9b7f3f9a73de1854c1
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2026-04-15 20:38:11 -0300

    Corrigindo implementação

--- src/HashTableExtensivel/EstruturasDeDados/Bucket.cs
+++ src/HashTableExtensivel/EstruturasDeDados/Bucket.cs
@@ -1,33 +1,75 @@
+using System.Text.Json.Serialization;
+
 namespace HashTableExtensivel.EstruturasDeDados;
 
 public class Bucket<TElemento>(int profundidade, int tamanho)
     where TElemento : IChaveável
 {
     public int Profundidade { get; private set; } = profundidade;
-    public bool Cheio => QuantidadeDeElementos == Tamanho;
+    public bool Cheio => Elementos.All(elemento => elemento is not null);
+    [JsonInclude]
     private TElemento?[] Elementos { get; init; } = new TElemento[tamanho];
-    private int QuantidadeDeElementos { get; set; } = 0;
     private int Tamanho { get; init; } = tamanho;
 
-    public void Inserir(int chave, TElemento elemento)
+    public void Inserir(TElemento elemento)
     {
-        if (Cheio)
-            throw new InvalidOperationException("O bucket já está cheio.");
-        var hash = chave % (int)Math.Pow(2, Profundidade);
-        Elementos[hash] = elemento;
-        QuantidadeDeElementos++;
+
+        for (int i = 0; i < Tamanho; i++)
+            if (Elementos[i] is null)
+            {
+                Elementos[i] = elemento;
+                return;
+            }
+
+        throw new InvalidOperationException("Não foi possível inserir o elemento no bucket.");
     }
 
     public TElemento? Buscar(int chave)
     {
-        var hash = chave % (int)Math.Pow(2, Profundidade);
-        return Elementos[hash];
+        foreach (var elemento in Elementos)
+        {
+            if (elemento is null)
+                continue;
+            if (elemento.ObterChave() == chave)
+                return elemento;
+        }
+        return default;
     }
 
     public void Remover(int chave)
     {
-        var hash = chave % (int)Math.Pow(2, Profundidade);
-        Elementos[hash] = default;
-        QuantidadeDeElementos--;
+        for (int i = 0; i < Tamanho; i++)
+        {
+            if (Elementos[i] is null)
+                continue;
+            if (Elementos[i]!.ObterChave() == chave)
+            {
+                Elementos[i] = default;
+                break;
+            }
+        }
+
+        throw new InvalidOperationException("Elemento não encontrado para remoção.");
+    }
+
+    public Bucket<TElemento> Dividir()
+    {
+        if (!Cheio)
+            throw new InvalidOperationException("Não é possível dividir um bucket que não está cheio.");
+
+        Profundidade++;
+        var novoBucket = new Bucket<TElemento>(Profundidade, Tamanho);
+        for (int i = 0; i < Tamanho; i++)
+        {
+            var elemento = Elementos[i]!;
+            var chave = elemento.ObterChave();
+            var bit = (chave >> (Profundidade - 1)) & 1;
+            if (bit == 0)
+                continue;
+            novoBucket.Inserir(elemento);
+            Elementos[i] = default;
+        }
+
+        return novoBucket;
     }
 }
--- src/HashTableExtensivel/EstruturasDeDados/HashTable.cs
+++ src/HashTableExtensivel/EstruturasDeDados/HashTable.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace HashTableExtensivel.EstruturasDeDados;
 
 public class HashTable<TElemento>
@@ -5,34 +8,80 @@ public class HashTable<TElemento>
 {
     public int TamanhoDoBucket { get; init; }
     public int Profundidade { get; private set; }
+    [JsonInclude]
     private Bucket<TElemento>[] Diretório { get; set; }
 
     public HashTable(int tamanhoDoBucket)
     {
         TamanhoDoBucket = tamanhoDoBucket;
         Profundidade = 0;
-        Diretório = new Bucket<TElemento>[Profundidade];
+        Diretório = new Bucket<TElemento>[1];
+        Diretório[0] = new Bucket<TElemento>(Profundidade, TamanhoDoBucket);
     }
 
-    private void Inserir(TElemento elemento)
+    public void Inserir(TElemento elemento)
     {
         var chave = elemento.ObterChave();
         var hash = chave % (int)Math.Pow(2, Profundidade);
+
+        Console.WriteLine($"Inserindo elemento com chave {chave} no bucket {hash}");
         var bucket = Diretório[hash];
-        bucket.Inserir(chave, elemento);
+
+        if (!bucket.Cheio)
+        {
+            bucket.Inserir(elemento);
+            return;
+        }
+
+        var novoBucket = bucket.Dividir();
+
+        if (bucket.Profundidade > Profundidade)
+        {
+            Profundidade++;
+
+            var tamanhoDoDiretórioAtual = Diretório.Length;
+            var tamanhoDoNovoDiretório = (int)Math.Pow(2, Profundidade);
+            var novoDiretório = new Bucket<TElemento>[tamanhoDoNovoDiretório];
+
+            for (int i = 0; i < Diretório.Length; i++)
+            {
+                novoDiretório[i] = Diretório[i];
+                novoDiretório[i + tamanhoDoDiretórioAtual] = Diretório[i];
+            }
+
+            Diretório = novoDiretório;
+        }
+
+
+        var mascara = (1 << novoBucket.Profundidade) - 1;
+        var bitNovo = 1 << (novoBucket.Profundidade - 1);
+        for (int i = 0; i < Diretório.Length; i++)
+        {
+            if (Diretório[i] == bucket && (i & mascara) >= bitNovo)
+                Diretório[i] = novoBucket;
+        }
+
+        Inserir(elemento);
     }
 
-    private TElemento? Buscar(int chave)
+    public TElemento? Buscar(int chave)
     {
         var hash = chave % (int)Math.Pow(2, Profundidade);
         var bucket = Diretório[hash];
+        Console.WriteLine($"Buscando elemento com chave {chave} no bucket {hash}");
         return bucket.Buscar(chave);
     }
 
-    private void Remover(int chave)
+    public void Remover(int chave)
     {
         var hash = chave % (int)Math.Pow(2, Profundidade);
         var bucket = Diretório[hash];
         bucket.Remover(chave);
+    }
+
+    public void Imprimir()
+    {
+        var json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true, IncludeFields = true });
+        Console.WriteLine(json);
     }
 }
